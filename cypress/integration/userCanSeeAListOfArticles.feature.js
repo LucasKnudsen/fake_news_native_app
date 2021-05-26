07b341baf5a2a0289c7c9fd69ab74b7101b1a5a7
kim_haaga <kim.haaga@live.se>
2021-05-26 16:59:40 +0200

    Add and setup cypress Add test for displaying article list Add fixture of articles

---
+++ cypress/integration/userCanSeeAListOfArticles.feature.js
@@ -0,0 +1,34 @@
+describe('User can view content of an article', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://fakest-newzz.herokuapp.com/api/articles', {
+      fixture: 'articles.json',
+    });
+    cy.visit('/');
+  });
+  it('is expected to display a hero section', () => {
+    cy.get('[data-testid=hero-article]')
+      .first()
+      .within(() => {
+        cy.get('[data-testid=title]').should(
+          'contain',
+          'Earth is a frisbee being thrown by God'
+        );
+        cy.get('[data-testid=category]').should('contain', 'Aliens');
+      });
+  });
+  it('is expected to display a list of 5 articles', () => {
+    cy.get('[data-testid=article]').should('have.length', 6);
+  });
+  it('is expected to display title, date, and author of article', () => {
+    cy.get('[data-testid=article]')
+      .second()
+      .within(() => {
+        cy.get('[data-testid=title]').should(
+          'contain',
+          'Canada SWAT team molest the Queen'
+        );
+        cy.get('[data-testid=date]').should('contain', '2021-05-25');
+        cy.get('[data-testid=author]').should('contain', 'By Mr. Fake');
+      });
+  });
+});
