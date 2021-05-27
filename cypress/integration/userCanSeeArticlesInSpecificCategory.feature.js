96117d9bedfd956a52aee0dfd4d491da06ca4100
arunbhalli <tomarbhalli@gmail.com>
2021-05-27 13:50:06 +0200

    Add test to assert list view in specific category

---
+++ cypress/integration/userCanSeeArticlesInSpecificCategory.feature.js
@@ -0,0 +1,29 @@
+describe('User can see articles in specific category', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://fakest-newzz.herokuapp.com/api/articles/1', {
+      fixture: 'articles.json',
+    });
+    cy.intercept('GET', 'https://fakest-newzz.herokuapp.com/api/articles/**', {
+      fixture: 'article.json',
+    });
+    cy.intercept('GET', 'https://fakest-newzz.herokuapp.com/api/articles/**', {
+      fixture: 'scienceCategories.json',
+    });
+    cy.viewport('iphone-x');
+    cy.visit('/');
+  });
+  describe('Successfully can see specific category', () => {
+    beforeEach(() => {
+      cy.get('[data-testid=article]').first().click();
+      cy.get('[data-testid=category-button]').click();
+    });
+    it('is expected to show two articles', () => {
+      cy.get('[data-testid=article]').should('have.length', 2);
+    });
+    it('is expected to show articles of science category', () => {
+      cy.get('[data-testid=category]').each(($span) => {
+        expect($span.text()).to.equal('Science');
+      });
+    });
+  });
+});
